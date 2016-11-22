62747e94dc5108b2dd5162642e04657cb57d24e0
Rosie Highsmith <ariel.highsmith@gmail.com>
2016-11-22 00:43:09 +0000

    spinWors solution for each loop

---
+++ reverseWord/reverseWordForEach.js
@@ -0,0 +1,12 @@
+function reverseWord(string) {
+  var array = string.split(' ');
+
+  array.forEach(function(word, index) {
+    if(word.length > 4) {
+      array[index] = word.split('').reverse().join('');
+    }
+  });
+  return array.join(' ');
+}
+
+new reverseWord('one two three fouuuuuur');
