9f76485561bcc360434e0d8399bf0e14eed358c6
Rosie Highsmith <ariel.highsmith@gmail.com>
2016-11-28 18:06:15 +0000

    Complete longestPalindrome.js

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -1,21 +1,36 @@
-function longestPalindrome(s) {
-  //find the longest palindrome in the string and 
-  //return the length of that palindrome
-  //loop through the string and use substring
-  //as you loop keep track of the longest substring so far
-  //from 0 index to s.length+1, add to array and check if palind.
-
-  for(var i = 0; i < s.length; i++) {
-    console.log(i);
-  }
-  //console.log(s.substring(0,2));
-  if(s == s.split('').reverse().join('')) {
-    console.log(s.length);
-  }
-  else {
-    console.log("There is no palindrome here!");
-  }
+//check if s is a palindrome, return true if it is, else false
+function isPalindrome(s) {
+  var rev_s = s.split("").reverse().join("");
+  return s == rev_s;
 }
 
-new longestPalindrome('aabaa');
-//new longestPalindrome('Johnny Depp')
+//return the length of the longest palindrome
+function longestPalindrome(s){
+  //default to 0 and empty longest_p
+  var longest_p_length = 0,
+  longest_p = '';
+  //loop thru string creating substrings i to end of string
+  for(var i=0; i < s.length; i++) {
+  var substring = s.substr(i, s.length);
+  //loop through substring creating sub_substring
+  //from beginning to j(substring length --> 0) 
+  for(var j=substring.length; j>=0; j--) {
+    var sub_subs = substring.substr(0, j);
+    if (sub_subs.length < 1) {
+      //skip iteration if subs_string length = 0
+      continue; } 
+      //assign longest_p_length to sub_subs if longest so far
+      if (isPalindrome(sub_subs)) {
+        if (sub_subs.length > longest_p_length) {
+          longest_p_length = sub_subs.length;
+          longest_p = sub_subs;
+        }
+      }
+    }
+  }
+  return longest_p_length;
+}
+
+console.log(longestPalindrome("abcxyzyxabcdaaa"));
+console.log(longestPalindrome("a"));
+console.log(longestPalindrome(""));
