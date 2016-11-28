d9ab5e901c34791ecf86d983f6a7d3b6ddd7ec3e
Rosie Highsmith <ariel.highsmith@gmail.com>
2016-11-28 22:11:51 +0000

    Completed map method buit with for loop

--- map.js
+++ map.js
@@ -3,16 +3,19 @@
 var _ = {
   // Implements:
   // https://lodash.com/docs#map
+  // => locks the value inside to the outside too
   map: (array, callback) => {
-    return 'FAIL :(';
+    var newArray = [];
+    for (var i = 0; i < array.length; i++) {
+      newArray.push(callback(array[i]));
+    }
+    return newArray;
   }
 }
-
 // Define a method that multiplies the input by 10
 function multiplyBy10(value) {
   return 10 * value;
 }
 const value = _.map([1, 2, 3], multiplyBy10);
-
 // value => [10, 20, 30]
 console.log(value);
